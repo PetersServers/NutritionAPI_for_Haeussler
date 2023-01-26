edce7c54df037879e35d83d99745af27eb1c2065
Peter Pichler <csav9172@student.uibk.ac.at>
2023-01-26 15:24:19 +0100

    added some comments to code

--- food_lists.py
+++ food_lists.py
@@ -3,17 +3,14 @@ from api_format import *
 import random
 from api_format import *
 import warnings
+from configurable_lists import *
 from faker import Faker
 
 #support functions for the input of the simplex model.
 #mainly works via lists
 
-choosen_foods = ["apple", "chicken", "salmon", "rice", "noodel", "couscous", "peas", "kebab", "broccoli", "kale"]
-foods_vegan = ["apricots", "tortellini", "pizza"]
-
-#["apple", "pizza", "peas", "broccoli", "kale", "spinach"]
-
 def call_stored_foods(vegan, num_items, random=False):
+    #function that returns lists based on specifications
     if vegan:
         return foods_vegan
     elif type(num_items) == str and random:
@@ -38,6 +35,7 @@ def get_prices(foods):
 
 
 def _create_foods_list_available(list_foods):
+    #function that tests if certain foods in a list are available in api data
     list_av_foods = []
     for i in list_foods:
         i = str(i).lower()
@@ -53,21 +51,23 @@ def _create_foods_list_available(list_foods):
         for name in list_av_foods:
             file.write(name + '\n')
 
-def generate_food_list(num):
-    # Initialize the Faker object
+def _generate_food_list(num):
+    #function that can be used to generate food list
+    #to backtest it against api
+    #to save data locally in json
     faker = Faker()
     # Create an empty list to store the food items
     food_items = []
-
-    # Use a for loop to generate 1000 food items
     for _ in range(num):
         food_items.append(faker.food().ingredient())
     food_items = set(food_items)
     _create_foods_list_available(food_items)
 
 
-
 def load_random_list(num_items):
+    #function that returns a random list of
+    #the foods that we have saved in the local
+    #txt file
     with open('ingr.txt', 'r') as f:
         grocery_list = f.readlines()
     grocery_list = [item.strip() for item in grocery_list]
@@ -75,12 +75,14 @@ def load_random_list(num_items):
     return random_list
 
 def load_all():
+    #crates a list of all food items
     with open('ingr.txt', 'r') as f:
         grocery_list = f.readlines()
     grocery_list = [item.strip() for item in grocery_list]
     return grocery_list
 
 def store_txt_json_locally():
+    #depracated code
     with open('ingr.txt', 'r') as f:
         grocery_list = f.readlines()
     grocery_list = [item.strip() for item in grocery_list]
